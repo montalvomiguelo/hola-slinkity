f00848b6346cff124fd39ce2b5f945d877e4778f
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-10-22 10:55:51 -0700

    Explore component pages

---
+++ src/about.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+export default {
+  props: ['contributors'],
+  frontMatter: {
+    title: 'A tragic tale',
+    layout: 'layout.njk'
+  }
+}
+
+</script>
+<template>
+  <article>
+    <h2>{{ title }}</h2>
+    <p>Did YOU ever hear the Tragedy of Darth Plagueis the Wise?</p>
+    <ul>
+      <li v-for="contributor in contributors" :key="contributor.ghProfile">
+        <a href="{{ contributor.ghProfile }}">{{ contributor.name }}</a>
+      </li>
+    </ul>
+  </article>
+</template>
